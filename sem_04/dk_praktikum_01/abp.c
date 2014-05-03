3de8dfb60eaab332df18135c0d0328d8ebe804e2
d4ryus <w.wackerbauer@yahoo.de>
2014-05-03 13:38:40 +0200

    changed code layout a bit

--- sem_04/dk_praktikum_01/abp.c
+++ sem_04/dk_praktikum_01/abp.c
@@ -1,3 +1,7 @@
+/* file: abp.c
+ * author: d4ryus - https://github.com/d4ryus/
+ * vim:foldmethod=syntax:foldcolumn=5:
+ */
 #include <sys/types.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -16,12 +20,16 @@ char rs[] = "Receiver sends: ";
 char message_sender[] = "TheQuickBrownFoxOle";
 char message_receiver[] = "JumpsOverTheLazyDog";
 
+int state = 0;
+int cursor = 0;
+
 /* child write, parrent read */
 int first_pipe[2];
 /* parrent write, child read */
 int second_pipe[2];
 
-void error(char *msg) {
+void error(char *msg)
+{
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
 }
@@ -31,40 +39,43 @@ void error(char *msg) {
  * write to first_pipe[1]
  * read from second_pipe[0] and
  */
-int sender(pid_t pid) {
-    int cursor = 0;
-    char control_bit = '0';
+int sender(pid_t pid)
+{
+    char control_bit;
     char msg[MSG_LEN];
+
+    control_bit = '0';
     msg[0] = message_sender[cursor];
     msg[1] = control_bit;
     msg[2] = '\0';
+    cursor++;
     write(second_pipe[1], msg, MSG_LEN);
     kill(pid, SIGUSR1);
-    int state = 1;
-    while(strlen(message_sender) > cursor) {
+
+    state = 1;
+    while(strlen(message_sender) > cursor)
+    {
         pause();
-        switch(state) {
+        switch(state)
+        {
             case 1:
                 control_bit = '1';
-                cursor++;
                 msg[0] = message_sender[cursor];
                 msg[1] = control_bit;
                 msg[2] = '\0';
-                write(second_pipe[1], msg, MSG_LEN);
-                kill(pid, SIGUSR1);
                 state = 2;
                 break;
             case 2:
                 control_bit = '0';
-                cursor++;
                 msg[0] = message_sender[cursor];
                 msg[1] = control_bit;
                 msg[2] = '\0';
-                write(second_pipe[1], msg, MSG_LEN);
-                kill(pid, SIGUSR1);
                 state = 1;
                 break;
         }
+        cursor++;
+        write(second_pipe[1], msg, MSG_LEN);
+        kill(pid, SIGUSR1);
     }
 }
 
@@ -73,102 +84,109 @@ int sender(pid_t pid) {
  * write to second_pipe[1]
  * read from first_pipe[0] and
  */
-int receiver() {
-    int cursor = 0;
-    int first = 1;
-    char control_bit = '0';
+int receiver()
+{
+    char control_bit;
     char msg[MSG_LEN];
-    int state = 1;
     int ppid = getppid();
-    while(strlen(message_receiver) > cursor) {
+    state = 1;
+    while(strlen(message_receiver) > cursor)
+    {
         pause();
-        switch(state) {
+        switch(state)
+        {
             case 1:
                 control_bit = '0';
-                cursor++;
-                if(first) {
-                    cursor = 0;
-                    first = 0;
-                }
                 msg[0] = message_receiver[cursor];
                 msg[1] = control_bit;
                 msg[2] = '\0';
-                write(first_pipe[1], msg, MSG_LEN);
-                kill(ppid, SIGUSR2);
                 state = 2;
                 break;
             case 2:
                 control_bit = '1';
-                cursor++;
                 msg[0] = message_receiver[cursor];
                 msg[1] = control_bit;
                 msg[2] = '\0';
-                write(first_pipe[1], msg, MSG_LEN);
-                kill(ppid, SIGUSR2);
                 state = 1;
                 break;
         }
+        cursor++;
+        write(first_pipe[1], msg, MSG_LEN);
+        kill(ppid, SIGUSR2);
     }
 }
 
-void abp_sender() {
+void abp_sender()
+{
     char msg_buf[BUF_LEN];
-    if(read(first_pipe[0], msg_buf, MSG_LEN) == -1) {
+
+    if(read(first_pipe[0], msg_buf, MSG_LEN) == -1)
         error("abp_sender read error");
-    }
+
     write(STDIN_FILENO, rs, sizeof(rs)-1);
     msg_buf[2] = '\n';
     write(STDIN_FILENO, msg_buf, sizeof(msg_buf));
 }
 
-void abp_receiver() {
+void abp_receiver()
+{
     char msg_buf[BUF_LEN];
-    if(read(second_pipe[0], msg_buf, MSG_LEN) == -1) {
+
+    if(read(second_pipe[0], msg_buf, MSG_LEN) == -1)
         error("abp_receiver read error");
-    }
+
     write(STDIN_FILENO, ss, sizeof(ss)-1);
     msg_buf[2] = '\n';
     write(STDIN_FILENO, msg_buf, sizeof(msg_buf));
 }
 
-int main() {
+int main()
+{
 
-    struct sigaction actionsender = {0};
-    struct sigaction actionreceiver = {0};
+    struct sigaction actionsender       = {0};
+    struct sigaction actionreceiver     = {0};
 
-    if(pipe(first_pipe) == -1 || pipe(second_pipe) == -1) {
+    if(pipe(first_pipe) == -1 || pipe(second_pipe) == -1)
         error("Can't open pipe");
-    }
 
     pid_t pid = fork();
-    if((pid < 0)) {
+
+    if((pid < 0))
         error("unexpected fork error:");
-    } else if (pid == 0) {
+    else if (pid == 0)
+    {
         /* child/receiver process */
         close(first_pipe[0]);       /* close pipes from parent prozess */
         close(second_pipe[1]);
+
         actionreceiver.sa_handler = abp_receiver;
         sigaction(SIGUSR1, &actionreceiver, 0);
+
         receiver();
+
         sleep(2);
         close(first_pipe[1]);
         close(second_pipe[0]);
         exit(0);
-    } else {
+    }
+    else
+    {
         /* parrent/sender processs */
         close(first_pipe[1]);       /* close pipes from child prozess */
         close(second_pipe[0]);
-        actionsender.sa_handler = abp_sender;
-        sigaction(SIGUSR2, &actionsender, 0);
+
+        actionsender.sa_handler       = abp_sender;
+        sigaction(SIGUSR2, &actionsender,       0);
+
         sleep(1);
         sender(pid);
+
         sleep(2);
         close(first_pipe[0]);
         close(second_pipe[1]);
         exit(0);
     }
     int pid_status;
-    if(waitpid(pid, &pid_status, 0) == -1) {
+    if(waitpid(pid, &pid_status, 0) == -1)
         error("Error waiting for child process");
-    }
 }
