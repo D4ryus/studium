e97cfec81e44a4737a0a58c6c48c566f232b4fa3
d4ryus <w.wackerbauer@yahoo.de>
2014-06-10 10:01:40 +0200

    changed codestyle

--- sem_04/bs_praktikum_05/mem.c
+++ sem_04/bs_praktikum_05/mem.c
@@ -9,8 +9,7 @@
 
 typedef struct _memblk memblk;
 
-typedef struct
-_memblk
+typedef struct _memblk
 {
     int pid;
     int size;
@@ -21,16 +20,13 @@ _memblk
 
 memblk* head = NULL;
 
-int
-add(const int id, const int alloc_size, memblk* ptr)
+int add(const int id, const int alloc_size, memblk* ptr)
 {
     if(ptr->alloc_size > 0)
         return -1;
 
-    if(ptr->size/2 >= alloc_size)
-    {
-        if(ptr->left == NULL)
-        {
+    if(ptr->size/2 >= alloc_size) {
+        if(ptr->left == NULL) {
             ptr->left                       = malloc(sizeof(memblk));
             ptr->left->size                 = ptr->size/2;
             ptr->left->alloc_size           = 0;
@@ -39,11 +35,11 @@ add(const int id, const int alloc_size, memblk* ptr)
             add(id, alloc_size, ptr->left);
             return 1;
         }
+
         if(add(id, alloc_size, ptr->left) != -1)
             return 1;
 
-        if(ptr->right == NULL)
-        {
+        if(ptr->right == NULL) {
             ptr->right             = malloc(sizeof(memblk));
             ptr->right->size       = ptr->size/2;
             ptr->right->alloc_size = 0;
@@ -52,14 +48,14 @@ add(const int id, const int alloc_size, memblk* ptr)
             add(id, alloc_size, ptr->right);
             return 1;
         }
+
         if(add(id, alloc_size, ptr->right) != -1)
             return 1;
 
         return -1;
     }
 
-    if((ptr->left == NULL) && (ptr->right == NULL))
-    {
+    if((ptr->left == NULL) && (ptr->right == NULL)) {
         ptr->alloc_size = alloc_size;
         ptr->pid = id;
         return 1;
@@ -68,55 +64,50 @@ add(const int id, const int alloc_size, memblk* ptr)
     return -1;
 }
 
-int
-print_mem(memblk* ptr)
+int print_mem(memblk* ptr)
 {
     int i;
-    if(ptr->alloc_size > 0)
-    {
+    if(ptr->alloc_size > 0) {
         for (i = 0; i < ptr->alloc_size; i++)
             printf("#");
         for (i = 0; i < (ptr->size - ptr->alloc_size); i++)
             printf("-");
         return 1;
     }
-    if(ptr->left != NULL)
-    {
+
+    if(ptr->left != NULL) {
         print_mem(ptr->left);
-    }
-    else
+    } else {
         for (i = 0; i < ptr->size/2; i++)
             printf("=");
+    }
 
     if((ptr->left != NULL) || (ptr->right != NULL))
         printf("|");
 
-    if(ptr->right != NULL)
+    if(ptr->right != NULL) {
         print_mem(ptr->right);
-    else
+    } else {
         for (i = 0; i < ptr->size/2; i++)
             printf("=");
+    }
 }
 
-int
-del(const int id, memblk* ptr)
+int del(const int id, memblk* ptr)
 {
-    if(ptr->left != NULL)
-    {
-        if(ptr->left->pid == id)
-        {
+    if(ptr->left != NULL) {
+        if(ptr->left->pid == id) {
             free(ptr->left);
             ptr->left = NULL;
             return 1;
         }
+
         if(del(id, ptr->left) != -1)
             return 1;
     }
 
-    if(ptr->right != NULL)
-    {
-        if(ptr->right->pid == id)
-        {
+    if(ptr->right != NULL) {
+        if(ptr->right->pid == id) {
             free(ptr->right);
             ptr->right = NULL;
             return 1;
@@ -124,11 +115,11 @@ del(const int id, memblk* ptr)
         if(del(id, ptr->right)!= -1)
             return 1;
     }
+
     return -1;
 }
 
-int
-rec_clean(memblk* ptr)
+int rec_clean(memblk* ptr)
 {
     if(ptr->left != NULL)
         rec_clean(ptr->left);
@@ -139,38 +130,34 @@ rec_clean(memblk* ptr)
     if((ptr->left == NULL) && (ptr->right == NULL))
         return 1;
 
-    if(ptr->left != NULL)
-    {
+    if(ptr->left != NULL) {
         if(    (ptr->left->alloc_size == 0)
             && (ptr->left->left       == NULL)
             && (ptr->left->right      == NULL))
         {
             free(ptr->left);
             ptr->left = NULL;
-        }
-        else
+        } else {
             rec_clean(ptr->left);
+        }
     }
 
-    if(ptr->right != NULL)
-    {
+    if(ptr->right != NULL) {
         if(    (ptr->right->alloc_size == 0)
             && (ptr->right->left       == NULL)
             && (ptr->right->right      == NULL))
         {
             free(ptr->right);
             ptr->right = NULL;
-        }
-        else
+        } else {
             rec_clean(ptr->right);
+        }
     }
 }
 
-int
-main(int argc, const char *argv[])
+int main(int argc, const char *argv[])
 {
-    if (argc <= 1)
-    {
+    if (argc <= 1) {
         printf("usage: ./mem [memory size]\n");
         exit(1);
     }
@@ -182,22 +169,17 @@ main(int argc, const char *argv[])
     int i;
     int x = 2;
     int flag = 0;
-    for (i = 0; i < 100; i++) // check if size is 2^x
-    {
-        if(x != size)
-        {
+    for (i = 0; i < 100; i++) {
+        if(x != size) {
             x *= 2;
             continue;
-        }
-        else
-        {
+        } else {
             flag = 1;
             break;
         }
     }
 
-    if(!flag)
-    {
+    if(!flag) {
         printf("entered value is not a acceptable memory size!\n");
         exit(1);
     }
@@ -214,8 +196,7 @@ main(int argc, const char *argv[])
     printf("[");
     print_mem(head);
     printf("]\n\n");
-    for (i = 1; i < 11; i++)
-    {
+    for (i = 1; i < 11; i++) {
         int value = rand() % (size/4) + 1;
         if(add(i, value, head) != -1)
             printf("added %d\n", value);
@@ -226,8 +207,7 @@ main(int argc, const char *argv[])
         printf("]\n\n");
     }
 
-    for (i = 10; i > 0; i--)
-    {
+    for (i = 10; i > 0; i--) {
         if(del(i, head) != -1)
             printf("deleted id %d\n", i);
         else
