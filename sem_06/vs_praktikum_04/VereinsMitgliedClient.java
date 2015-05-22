6c6991775dab8e69da9a11d1a4ccb1d41e1b3d9c
d4ryus <w.wackerbauer@yahoo.de>
2015-05-22 12:19:30 +0200

    fixed indent

--- sem_06/vs_praktikum_04/VereinsMitgliedClient.java
+++ sem_06/vs_praktikum_04/VereinsMitgliedClient.java
@@ -11,55 +11,44 @@ import Verein.VereinsMitglied;
 import Verein.VereinsMitgliedHelper;
 
 public class VereinsMitgliedClient {
-
 	public static void main(String args[]) {
 		try {
 			ORB orb = ORB.init(args, null);
 			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
 			NamingContext ncRef = NamingContextHelper.narrow(objRef);
-			
+
 			NameComponent city[] = { new NameComponent("muenchen", "") };
 			ncRef = NamingContextHelper.narrow(ncRef.resolve(city));
-			
+
 			NameComponent club[] = { new NameComponent("fussball", "") };
 			ncRef = NamingContextHelper.narrow(ncRef.resolve(club));
-			
-			NameComponent serv[] = { new NameComponent("franz", "") };
-			
-	        BindingListHolder bl = new BindingListHolder();
-	        BindingIteratorHolder blIt= new BindingIteratorHolder();
-	        
-	        ncRef.list(1000, bl, blIt);
-	        Binding bindings[] = bl.value;
-	        if (bindings.length == 0) {
-	        	return;
-	        }
-	        
-	        for (int i=0; i < bindings.length; i++) {
-	            // get the object reference for each binding
-	            org.omg.CORBA.Object obj = ncRef.resolve(bindings[i].binding_name);
-	            String objStr = orb.object_to_string(obj);
-	            int lastIx = bindings[i].binding_name.length-1;
 
-	            // check to see if this is a naming context
-	            if (bindings[i].binding_type == BindingType.ncontext) {
-	              System.out.println( "Context: " +
-	                  bindings[i].binding_name[lastIx].id);
-	            } else {
-	                System.out.println("Object: " +
-	                    bindings[i].binding_name[lastIx].id);
-	            }
-	        }
-			//objRef = ncRef.resolve(serv);
-		
-			//VereinsMitglied vereinMitgliedRef = VereinsMitgliedHelper.narrow(objRef);
-			//vereinMitgliedRef.erhoeheBeitrag("fussball", (short) 600);
-			
-			//System.out.println("Name: " + vereinMitgliedRef.mname());
+			NameComponent serv[] = { new NameComponent("franz", "") };
+
+			BindingListHolder bl = new BindingListHolder();
+			BindingIteratorHolder blIt= new BindingIteratorHolder();
+
+			ncRef.list(1000, bl, blIt);
+			Binding bindings[] = bl.value;
+
+			if (bindings.length == 0) {
+				return;
+			}
+
+			for (int i = 0; i < bindings.length; i++) {
+				org.omg.CORBA.Object obj = ncRef.resolve(bindings[i].binding_name);
+				String objStr = orb.object_to_string(obj);
+				int lastIx = bindings[i].binding_name.length - 1;
+
+				if (bindings[i].binding_type == BindingType.ncontext) {
+					System.out.println("Context: " + bindings[i].binding_name[lastIx].id);
+				} else {
+					System.out.println("Object: " + bindings[i].binding_name[lastIx].id);
+				}
+			}
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
 			e.printStackTrace(System.out);
 		}
 	}
-
 }
